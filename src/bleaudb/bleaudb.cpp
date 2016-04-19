77a4c05bb1e1a4674455d516822de8aef18f3bb6
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2016-04-19 17:23:04 +0200

    implemented bleaudb

---
+++ src/bleaudb/bleaudb.cpp
@@ -0,0 +1,539 @@
+/***************************************************************************************************
+**
+** $QTCARTO_BEGIN_LICENSE:GPL3$
+**
+** Copyright (C) 2016 Fabrice Salvaire
+** Contact: http://www.fabrice-salvaire.fr
+**
+** This file is part of the QtCarto library.
+**
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**
+** $QTCARTO_END_LICENSE$
+**
+***************************************************************************************************/
+
+/**************************************************************************************************/
+
+#include "bleaudb.h"
+
+// QC_BEGIN_NAMESPACE
+
+/**************************************************************************************************/
+
+BleauPlace::BleauPlace(QObject * parent)
+  : QObject(parent),
+    m_coordinate(),
+    m_name(),
+    m_category(),
+    m_note()
+{}
+
+BleauPlace::BleauPlace(const BleauPlace & other, QObject * parent)
+  : QObject(parent),
+    m_coordinate(other.m_coordinate),
+    m_name(other.m_name),
+    m_category(other.m_category),
+    m_note(other.m_note)
+{}
+
+BleauPlace::~BleauPlace()
+{}
+
+BleauPlace &
+BleauPlace::operator=(const BleauPlace & other)
+{
+  if (this != &other) {
+    m_coordinate = other.m_coordinate;
+    m_name = other.m_name;
+    m_category = other.m_category;
+    m_note = other.m_note;
+  }
+
+  return *this;
+}
+
+void
+BleauPlace::set_coordinate(const QGeoCoordinate & coordinate)
+{
+  m_coordinate = coordinate;
+  emit coordinateChanged();
+}
+
+void
+BleauPlace::set_name(const QString & name)
+{
+  m_name = name;
+  emit nameChanged();
+}
+
+void
+BleauPlace::set_category(BleauPlaceCategory category)
+{
+  m_category = category;
+  emit categoryChanged();
+}
+
+void
+BleauPlace::set_note(const QString & note)
+{
+  m_note = note;
+  emit noteChanged();
+}
+
+/**************************************************************************************************/
+
+BleauMassif::BleauMassif(QObject * parent)
+  : QObject(parent),
+    m_coordinate(),
+    m_name(),
+    m_access(),
+    m_alternative_name(),
+    m_chaos_type(),
+    m_note(),
+    m_parcelles(),
+    m_rdv(),
+    m_secteur(),
+    m_velo()
+{}
+
+BleauMassif::BleauMassif(const BleauMassif & other, QObject * parent)
+  : QObject(parent),
+    m_coordinate(other.m_coordinate),
+    m_name(other.m_name),
+    m_access(other.m_access),
+    m_alternative_name(other.m_alternative_name),
+    m_chaos_type(other.m_chaos_type),
+    m_note(other.m_note),
+    m_parcelles(other.m_parcelles),
+    m_rdv(other.m_rdv),
+    m_secteur(other.m_secteur),
+    m_velo(other.m_velo)
+{}
+
+BleauMassif::~BleauMassif()
+{}
+
+BleauMassif &
+BleauMassif::operator=(const BleauMassif & other)
+{
+  if (this != &other) {
+    m_coordinate = other.m_coordinate;
+    m_name = other.m_name;
+    m_access = other.m_access;
+    m_alternative_name = other.m_alternative_name;
+    m_chaos_type = other.m_chaos_type;
+    m_note = other.m_note;
+    m_parcelles = other.m_parcelles;
+    m_rdv = other.m_rdv;
+    m_secteur = other.m_secteur;
+    m_velo = other.m_velo;
+  }
+
+  return *this;
+}
+
+void
+BleauMassif::set_coordinate(const QGeoCoordinate & coordinate)
+{
+  m_coordinate = coordinate;
+  emit coordinateChanged();
+}
+
+void
+BleauMassif::set_name(const QString & name)
+{
+  m_name = name;
+  emit nameChanged();
+}
+
+void
+BleauMassif::set_access(const QString & access)
+{
+  m_access = access;
+  emit accessChanged();
+}
+
+void
+BleauMassif::set_alternative_name(const QString & alternative_name)
+{
+  m_alternative_name = alternative_name;
+  emit alternative_nameChanged();
+}
+
+void
+BleauMassif::set_chaos_type(BleauChaosType chaos_type)
+{
+  m_chaos_type = chaos_type;
+  emit chaos_typeChanged();
+}
+
+void
+BleauMassif::set_note(const QString & note)
+{
+  m_note = note;
+  emit noteChanged();
+}
+
+void
+BleauMassif::set_parcelles(const QString & parcelles)
+{
+  m_parcelles = parcelles;
+  emit parcellesChanged();
+}
+
+void
+BleauMassif::set_rdv(const QString & rdv)
+{
+  m_rdv = rdv;
+  emit rdvChanged();
+}
+
+void
+BleauMassif::set_secteur(const QString & secteur)
+{
+  m_secteur = secteur;
+  emit secteurChanged();
+}
+
+void
+BleauMassif::set_velo(const QString & velo)
+{
+  m_velo = velo;
+  emit veloChanged();
+}
+
+/**************************************************************************************************/
+
+BleauBoulder::BleauBoulder(QObject * parent)
+  : QObject(parent),
+    m_coordinate(),
+    m_name(),
+    m_comment(),
+    m_grade(),
+    m_number()
+{}
+
+BleauBoulder::BleauBoulder(const BleauBoulder & other, QObject * parent)
+  : QObject(parent),
+    m_coordinate(other.m_coordinate),
+    m_name(other.m_name),
+    m_comment(other.m_comment),
+    m_grade(other.m_grade),
+    m_number(other.m_number)
+{}
+
+BleauBoulder::~BleauBoulder()
+{}
+
+BleauBoulder &
+BleauBoulder::operator=(const BleauBoulder & other)
+{
+  if (this != &other) {
+    m_coordinate = other.m_coordinate;
+    m_name = other.m_name;
+    m_comment = other.m_comment;
+    m_grade = other.m_grade;
+    m_number = other.m_number;
+  }
+
+  return *this;
+}
+
+void
+BleauBoulder::set_coordinate(const QGeoCoordinate & coordinate)
+{
+  m_coordinate = coordinate;
+  emit coordinateChanged();
+}
+
+void
+BleauBoulder::set_name(const QString & name)
+{
+  m_name = name;
+  emit nameChanged();
+}
+
+void
+BleauBoulder::set_comment(const QString & comment)
+{
+  m_comment = comment;
+  emit commentChanged();
+}
+
+void
+BleauBoulder::set_grade(BleauGrade grade)
+{
+  m_grade = grade;
+  emit gradeChanged();
+}
+
+void
+BleauBoulder::set_number(BleauWayNumber number)
+{
+  m_number = number;
+  emit numberChanged();
+}
+
+/**************************************************************************************************/
+
+BleauCircuit::BleauCircuit(QObject * parent)
+  : QObject(parent),
+    m_coordinate(),
+    m_boulders(),
+    m_colour(),
+    m_creation_date(),
+    m_gestion(),
+    m_grade(),
+    m_massif(),
+    m_note(),
+    m_number(),
+    m_opener(),
+    m_refection_date(),
+    m_refection_note(),
+    m_status(),
+    m_topos()
+{}
+
+BleauCircuit::BleauCircuit(const BleauCircuit & other, QObject * parent)
+  : QObject(parent),
+    m_coordinate(other.m_coordinate),
+    m_boulders(other.m_boulders),
+    m_colour(other.m_colour),
+    m_creation_date(other.m_creation_date),
+    m_gestion(other.m_gestion),
+    m_grade(other.m_grade),
+    m_massif(other.m_massif),
+    m_note(other.m_note),
+    m_number(other.m_number),
+    m_opener(other.m_opener),
+    m_refection_date(other.m_refection_date),
+    m_refection_note(other.m_refection_note),
+    m_status(other.m_status),
+    m_topos(other.m_topos)
+{}
+
+BleauCircuit::~BleauCircuit()
+{}
+
+BleauCircuit &
+BleauCircuit::operator=(const BleauCircuit & other)
+{
+  if (this != &other) {
+    m_coordinate = other.m_coordinate;
+    m_boulders = other.m_boulders;
+    m_colour = other.m_colour;
+    m_creation_date = other.m_creation_date;
+    m_gestion = other.m_gestion;
+    m_grade = other.m_grade;
+    m_massif = other.m_massif;
+    m_note = other.m_note;
+    m_number = other.m_number;
+    m_opener = other.m_opener;
+    m_refection_date = other.m_refection_date;
+    m_refection_note = other.m_refection_note;
+    m_status = other.m_status;
+    m_topos = other.m_topos;
+  }
+
+  return *this;
+}
+
+void
+BleauCircuit::set_coordinate(const QGeoCoordinate & coordinate)
+{
+  m_coordinate = coordinate;
+  emit coordinateChanged();
+}
+
+// void
+// BleauCircuit::set_boulders(BleauBoulderList boulders)
+// {
+//   m_boulders = boulders;
+//   emit bouldersChanged();
+// }
+
+void
+BleauCircuit::append_boulder(const BleauBoulder & boulder)
+{
+  m_boulders.append(boulder);
+  // emit bouldersChanged();
+}
+
+void
+BleauCircuit::set_colour(const QString & colour)
+{
+  m_colour = colour;
+  emit colourChanged();
+}
+
+void
+BleauCircuit::set_creation_date(unsigned int creation_date)
+{
+  m_creation_date = creation_date;
+  emit creation_dateChanged();
+}
+
+void
+BleauCircuit::set_gestion(const QString & gestion)
+{
+  m_gestion = gestion;
+  emit gestionChanged();
+}
+
+void
+BleauCircuit::set_grade(BleauAlpineGrade grade)
+{
+  m_grade = grade;
+  emit gradeChanged();
+}
+
+void
+BleauCircuit::set_massif(BleauMassif massif)
+{
+  m_massif = massif;
+  emit massifChanged();
+}
+
+void
+BleauCircuit::set_note(const QString & note)
+{
+  m_note = note;
+  emit noteChanged();
+}
+
+void
+BleauCircuit::set_number(unsigned int number)
+{
+  m_number = number;
+  emit numberChanged();
+}
+
+void
+BleauCircuit::set_opener(BleauOpeners opener)
+{
+  m_opener = opener;
+  emit openerChanged();
+}
+
+void
+BleauCircuit::set_refection_date(unsigned int refection_date)
+{
+  m_refection_date = refection_date;
+  emit refection_dateChanged();
+}
+
+void
+BleauCircuit::set_refection_note(BleauRefectionNote refection_note)
+{
+  m_refection_note = refection_note;
+  emit refection_noteChanged();
+}
+
+void
+BleauCircuit::set_status(const QString & status)
+{
+  m_status = status;
+  emit statusChanged();
+}
+
+void
+BleauCircuit::set_topos(const QList<QString> & topos)
+{
+  m_topos = topos;
+  emit toposChanged();
+}
+
+/**************************************************************************************************/
+
+BleauDB::BleauDB(QObject * parent)
+  : QObject(parent),
+    m_places(),
+    m_massifs(),
+    m_circuits()
+{}
+
+// BleauDB::BleauDB(const BleauDB & other, QObject * parent)
+//   : QObject(parent),
+//     m_places(other.m_places),
+//     m_massifs(other.m_massifs),
+//     m_circuits(other.m_circuits)
+// {}
+
+BleauDB::~BleauDB()
+{}
+
+// BleauDB &
+// BleauDB::operator=(const BleauDB & other)
+// {
+//   if (this != &other) {
+//     m_places = other.m_places;
+//     m_massifs = other.m_massifs;
+//     m_circuits = other.m_circuits;
+//   }
+
+//   return *this;
+// }
+
+// void
+// BleauDB::set_places(const QList<BleauPlace> & places)
+// {
+//   m_places = places;
+//   emit placesChanged();
+// }
+
+// void
+// BleauDB::set_massifs(const QList<BleauMassif> & massifs)
+// {
+//   m_massifs = massifs;
+//   emit massifsChanged();
+// }
+
+// void
+// BleauDB::set_circuits(const QList<BleauCircuit> & circuits)
+// {
+//   m_circuits = circuits;
+//   emit circuitsChanged();
+// }
+
+void
+BleauDB::append_place(const BleauPlace & place)
+{
+  m_places.append(place);
+  // emit placesChanged();
+}
+
+void
+BleauDB::append_massif(const BleauMassif & massif)
+{
+  m_massifs.append(massif);
+  // emit massifsChanged();
+}
+
+void
+BleauDB::append_circuit(const BleauCircuit & circuit)
+{
+  m_circuits.append(circuit);
+  // emit circuitsChanged();
+}
+
+/**************************************************************************************************/
+
+// QC_END_NAMESPACE
+
+/***************************************************************************************************
+ *
+ * End
+ *
+ **************************************************************************************************/
---
+++ src/bleaudb/bleaudb_json_loader.cpp
@@ -0,0 +1,138 @@
+/***************************************************************************************************
+**
+** $QTCARTO_BEGIN_LICENSE:GPL3$
+**
+** Copyright (C) 2016 Fabrice Salvaire
+** Contact: http://www.fabrice-salvaire.fr
+**
+** This file is part of the QtCarto library.
+**
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**
+** $QTCARTO_END_LICENSE$
+**
+***************************************************************************************************/
+
+/**************************************************************************************************/
+
+#include <stdexcept>
+
+#include "bleaudb_json_loader.h"
+
+#include <QByteArray>
+#include <QFile>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonValue>
+#include <QtDebug>
+
+/**************************************************************************************************/
+
+// QC_BEGIN_NAMESPACE
+
+/**************************************************************************************************/
+
+// QcBleauDbJsonLoader::QcBleauDbJsonLoader(const QString & json_path, BleauDB & bleaudb)
+void
+load_json_bleaudb(const QString & json_path, BleauDB & bleaudb)
+{
+  QFile json_file(json_path);
+
+  if (!json_file.open(QIODevice::ReadOnly))
+    throw std::invalid_argument("Couldn't open file.");
+
+  QByteArray json_data = json_file.readAll();
+  QJsonDocument json_document(QJsonDocument::fromJson(json_data));
+  QJsonObject root = json_document.object();
+
+  QJsonArray json_places = root[QStringLiteral("places")].toArray();
+  for (const QJsonValue & json_value : json_places) {
+    QJsonObject json_object = json_value.toObject();
+    BleauPlace place;
+    double longitude = json_object[QStringLiteral("longitude")].toDouble();
+    double latitude = json_object[QStringLiteral("latitude")].toDouble();
+    place.set_coordinate(QGeoCoordinate(latitude, longitude));
+    place.set_name(json_object[QStringLiteral("name")].toString());
+    // place.set_category(json_object[QStringLiteral("category")].toString());
+    place.set_note(json_object[QStringLiteral("note")].toString());
+    bleaudb.append_place(place);
+  }
+
+  QJsonArray json_massifs = root[QStringLiteral("massifs")].toArray();
+  for (const QJsonValue & json_value : json_massifs) {
+    QJsonObject json_object = json_value.toObject();
+    BleauMassif massif;
+    double longitude = json_object[QStringLiteral("longitude")].toDouble();
+    double latitude = json_object[QStringLiteral("latitude")].toDouble();
+    massif.set_coordinate(QGeoCoordinate(latitude, longitude));
+    massif.set_name(json_object[QStringLiteral("name")].toString());
+    massif.set_access(json_object[QStringLiteral("access")].toString());
+    massif.set_alternative_name(json_object[QStringLiteral("alternative_name")].toString());
+    // massif.set_chaos_type(json_object[QStringLiteral("chaos_type")].toString());
+    massif.set_note(json_object[QStringLiteral("note")].toString());
+    massif.set_parcelles(json_object[QStringLiteral("parcelles")].toString());
+    massif.set_rdv(json_object[QStringLiteral("rdv")].toString());
+    massif.set_secteur(json_object[QStringLiteral("secteur")].toString());
+    massif.set_velo(json_object[QStringLiteral("velo")].toString());
+    bleaudb.append_massif(massif);
+  }
+
+  QJsonArray json_circuits = root[QStringLiteral("circuits")].toArray();
+  for (const QJsonValue & json_value : json_circuits) {
+    QJsonObject json_object = json_value.toObject();
+    BleauCircuit circuit;
+    double longitude = json_object[QStringLiteral("longitude")].toDouble();
+    double latitude = json_object[QStringLiteral("latitude")].toDouble();
+    circuit.set_coordinate(QGeoCoordinate(latitude, longitude));
+    circuit.set_colour(json_object[QStringLiteral("colour")].toString());
+    circuit.set_creation_date(json_object[QStringLiteral("creation_date")].toInt());
+    circuit.set_gestion(json_object[QStringLiteral("gestion")].toString());
+    // circuit.set_grade(json_object[QStringLiteral("grade")].toString());
+    // circuit.set_massif(json_object[QStringLiteral("massif")].toString());
+    circuit.set_note(json_object[QStringLiteral("note")].toString());
+    circuit.set_number(json_object[QStringLiteral("number")].toInt());
+    // circuit.set_opener(json_object[QStringLiteral("opener")].toString());
+    circuit.set_refection_date(json_object[QStringLiteral("refection_date")].toInt());
+    // circuit.set_refection_note(json_object[QStringLiteral("refection_note")].toString());
+    circuit.set_status(json_object[QStringLiteral("status")].toString());
+    // circuit.set_topos(json_object[QStringLiteral("topos")].toString());
+
+    QJsonArray json_boulders = json_object[QStringLiteral("boulders")].toArray();
+    for (const QJsonValue & json_value : json_boulders) {
+      QJsonObject json_object = json_value.toObject();
+      BleauBoulder boulder;
+      double longitude = json_object[QStringLiteral("longitude")].toDouble();
+      double latitude = json_object[QStringLiteral("latitude")].toDouble();
+      circuit.set_coordinate(QGeoCoordinate(latitude, longitude));
+      boulder.set_name(json_object[QStringLiteral("name")].toString());
+      boulder.set_comment(json_object[QStringLiteral("comment")].toString());
+      // boulder.set_grade(json_object[QStringLiteral("grade")].toString());
+      // boulder.set_number(json_object[QStringLiteral("number")].toString());
+      // boulders.append(boulder);
+      circuit.append_boulder(boulder);
+    }
+    // circuit.set_boulders(boulders);
+
+    bleaudb.append_circuit(circuit);
+  }
+}
+
+// QC_END_NAMESPACE
+
+/***************************************************************************************************
+ *
+ * End
+ *
+ **************************************************************************************************/
