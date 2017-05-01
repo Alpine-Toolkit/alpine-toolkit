33fa37d39508e58fb3cedce7a9cb768b1d3470b6
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2017-05-01 14:05:28 +0200

    added profiling option to cmake script

--- script/run-cmake.sh
+++ script/run-cmake.sh
@@ -6,12 +6,17 @@ append_to_ld_library_path_if_not /opt/Qt/${QT_VERSION}/gcc_64/lib/
 
 export CFLAGS="-g -O0"
 export CXXFLAGS="${CFLAGS}"
-BUILD_TYPE=Debug
+BUILD_TYPE=DEBUG
 
+# https://gcc.gnu.org/onlinedocs/gcc/Optimize-Options.html
 # set_gcc_optimisation
-# BUILD_TYPE=Version
+# export CFLAGS="-O3 -march=broadwell -mtune=broadwell -flto"
+# export CXXFLAGS="${CFLAGS}"
+# BUILD_TYPE=RELEASE
 
 cmake \
   -D CMAKE_BUILD_TYPE:STRING=={BUILD_TYPE} \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+  -DSANITIZE=OFF \
+  -DINSTRUMENT_FUNTIONS=OFF \
   /home/fabrice/home/developpement/qtcarto-application/alpine-toolkit
