9ba0268d65b82d12b1924f430c0913e2df89859d
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2017-08-26 13:55:50 +0200

    run-cmake.sh

--- scripts/run-cmake.sh
+++ scripts/run-cmake.sh
@@ -1,10 +1,15 @@
+#! /bin/bash
+
+####################################################################################################
+
+source_path=/home/fabrice/home/developpement/qtcarto-application/alpine-toolkit
+build_path=${source_path}/build-cmake
+
 # QT_VERSION=5.7
 # QT_VERSION=5.8
-# QT_VERSION=5.9
-# QT_VERSION=5.9.1
-QT_VERSION=5.10.0
-append_to_path_if_not /opt/Qt/${QT_VERSION}/gcc_64/bin/
-append_to_ld_library_path_if_not /opt/Qt/${QT_VERSION}/gcc_64/lib/
+#QT_VERSION=5.9
+QT_VERSION=5.9.1
+# QT_VERSION=5.10.0
 
 export CFLAGS="-g -O0"
 export CXXFLAGS="${CFLAGS}"
@@ -16,9 +21,21 @@ BUILD_TYPE=DEBUG
 # export CXXFLAGS="${CFLAGS}"
 # BUILD_TYPE=RELEASE
 
+####################################################################################################
+
+source $HOME/.bashrc
+
+append_to_path_if_not /opt/Qt/${QT_VERSION}/gcc_64/bin/
+append_to_ld_library_path_if_not /opt/Qt/${QT_VERSION}/gcc_64/lib/
+
+if ! [ -e ${build_path} ]; then
+  mkdir ${build_path}
+fi
+pushd ${build_path}
+
 cmake \
   -D CMAKE_BUILD_TYPE:STRING=={BUILD_TYPE} \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
   -DSANITIZE=OFF \
   -DINSTRUMENT_FUNTIONS=OFF \
-  /home/fabrice/home/developpement/qtcarto-application/alpine-toolkit
+  ${source_path}
