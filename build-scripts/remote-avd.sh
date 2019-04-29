58d1b388b0ab02fe4fbc2bb8cefebb20b88b06d2
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2019-04-29 17:04:57 +0200

    moved build scripts

---
+++ build-scripts/remote-avd.sh
@@ -0,0 +1,15 @@
+# Launch avd on remote
+# emulator -avd Nexus_5x_API_23
+
+REMOTE=192.168.10.1
+PORT1=5554
+PORT2=5555
+
+# Forward local ports to remote ports
+# -N  Do not execute a remote command.
+# -L local_port:host:host_port
+# ask password !
+ssh -N -L ${PORT1}:localhost:${PORT1} -L ${PORT2}:localhost:${PORT2} ${REMOTE} &
+
+adb kill-server # to restart server
+adb devices -l
