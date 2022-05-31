7b5363af7b424143fe29da03faa5fd73d9260522
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2022-05-31 12:54:33 +0200

    Add doc task

--- tasks/doc.py
+++ tasks/doc.py
@@ -26,20 +26,31 @@
 
 ####################################################################################################
 
-from types import ModuleType
+from pathlib import Path
+import os
 
-# http://www.pyinvoke.org
-from invoke import Collection
+from invoke import task
 
 ####################################################################################################
 
-from . import build
-from . import clean
-from . import doc
-from . import ign
-from . import inspect
+@task
+def set_source_path(ctx):
+    source_path = Path(__file__).parents[1]
+    ctx.doc_path = source_path.joinpath('doc')
 
-modules = [obj for name, obj in globals().items() if isinstance(obj, ModuleType)]
-ns = Collection()
-for _ in modules:
-    ns.add_collection(Collection.from_module(_))
+####################################################################################################
+
+@task(set_source_path)
+def doxygen(ctx):
+    doxyfile = ctx.doc_path.joinpath('doxygen', 'Doxyfile')
+    ctx.run(f'doxygen {doxyfile}')
+
+@task(set_source_path)
+def open_doxygen(ctx):
+    index = ctx.doc_path.joinpath('doxygen', 'build', 'html', 'index.html')
+    ctx.run(f'xdg-open {index}')
+
+@task(set_source_path)
+def spinx(ctx):
+    make = ctx.doc_path.joinpath('sphinx', 'make-html')
+    ctx.run(make)
